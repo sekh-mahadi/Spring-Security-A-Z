a2d03748026be976fc3411fb4e33092d1d9ff60c
Sekh <sekh-mahadi>
2022-02-21 15:26:57 +0600

    Authentication Provider and Implementing it

---
+++ Spring-Security_v2/src/main/java/io/mtech/config/BankUsernamePwdAuthenticationProvider.java
@@ -0,0 +1,50 @@
+package io.mtech.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import io.mtech.model.Customer;
+import io.mtech.repository.CustomerRepository;
+
+@Component
+public class BankUsernamePwdAuthenticationProvider implements AuthenticationProvider {
+	@Autowired
+	private CustomerRepository customerRepository;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@Override
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		String userName = authentication.getName();
+		String pwd = authentication.getCredentials().toString();
+		List<Customer> customers = customerRepository.findByEmail(userName);
+		if (customers.size() > 0) {
+			if (passwordEncoder.matches(pwd, customers.get(0).getPwd())) {
+				List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+				authorities.add(new SimpleGrantedAuthority(customers.get(0).getRole()));
+				return new UsernamePasswordAuthenticationToken(userName, pwd, authorities);
+			} else {
+				throw new BadCredentialsException("Invalid password!");
+			}
+		} else {
+			throw new BadCredentialsException("No user registered with this details!");
+		}
+	}
+
+	@Override
+	public boolean supports(Class<?> authenticationType) {
+		return authenticationType.equals(UsernamePasswordAuthenticationToken.class);
+	}
+
+}
