78bdbf6f7a4efdadcd84ae96b0e8891548cf551f
Sekh <sekh-mahadi>
2022-03-17 11:47:01 +0600

    Configure Role Base Authentication.

--- Spring-Security_v2/src/main/resources/data.sql
+++ Spring-Security_v2/src/main/resources/data.sql
@@ -66,4 +66,6 @@ INSERT INTO `authorities` (`customer_id`, `name`)
 INSERT INTO `authorities` (`customer_id`, `name`)
  VALUES (1, 'WRITE');
  
+ UPDATE authorities SET name="ROLE_USER" WHERE id=1;
+UPDATE authorities SET name="ROLE_ADMIN" WHERE id=2;
  
